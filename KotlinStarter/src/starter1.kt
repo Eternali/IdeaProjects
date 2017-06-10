a6200ac80d4cee4d09e3825b22f41bd06dd783f6
conrad <conrad.heidebrecht@gmail.com>
2017-06-10 15:30:18 -0400

    test

--- KotlinStarter/src/starter1.kt
+++ KotlinStarter/src/starter1.kt
@@ -5,10 +5,24 @@
 package KotlinStarter
 
 fun main (args: Array<String>) {
+
+
+
+    /*
+    var letterChar : Char = 'A'
+    println("A is char : ${letterChar is Char}")
+
+    if (true is Boolean) {
+        print("true is boolean\n")
+    }
+
     println("Hello world")
 
     val name = "conrad"
     var bigInt: Int = Int.MAX_VALUE
     var smallInt: Int = Int.MIN_VALUE
-    
+
+    println("Biggest int : " + bigInt)
+    println("Smallest int : $smallInt")
+    */
 }
