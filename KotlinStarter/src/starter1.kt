11d91694f1d9418097e888e7b6c7a5478a4bb5c9
conrad <conrad.heidebrecht@gmail.com>
2017-06-10 15:16:08 -0400

    initial commit

---
+++ KotlinStarter/src/starter1.kt
@@ -0,0 +1,14 @@
+/**
+ * Created by Conrad Heidebrecht
+ */
+
+package KotlinStarter
+
+fun main (args: Array<String>) {
+    println("Hello world")
+
+    val name = "conrad"
+    var bigInt: Int = Int.MAX_VALUE
+    var smallInt: Int = Int.MIN_VALUE
+    
+}
